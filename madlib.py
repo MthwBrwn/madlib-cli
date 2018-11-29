adba6f18a763fd3f457baa72e96cb170ccc616b7
mthwbrwn <mthwbrwn@gmail.com>
2018-11-28 20:58:38 -0800

    adding function to read files

--- madlib.py
+++ madlib.py
@@ -1,23 +1,40 @@
 import json
+import re
 
 # need to parse questions and serialize the users questions into a answerlist
-def word_generator():
+def word_open():
+
     """ word generator
     """
+    get_text = open("data.txt", "rt")
+    contents = get_text.read()
+    get_text.close()
+    print(contents)
+
+
 def greeting():
-    pass
+
+
+    print('Hello welcome to our MadLib game.')
+    print('You will be asked a series of questions.')
+    print('Please reply to each question once prompted.')
+    print('Once you are done a MadLib page will be returned to you')
+    print ('Lets get started')
 
 
 # need an out going message stating game and asking questions
 
-print('Hello welcome to our MadLib game.')
-print('You will be asked a series of questions.')
-print('Please reply to each question once prompted.')
-print('Once you are done a MadLib page will be returned to you')
-print ('Lets get started')
+def run():
 
-word_generator():
-
+    greeting()
+    word_open()
 
 
 # need to read text back to user with user inputs in place of {questions}
+
+# need to parse info from our data file into a list of strings and then insert
+#user input into list at
+
+
+if __name__ =='__main__':
+    run()
--- test_madlib.py
+++ test_madlib.py
@@ -1,9 +1,18 @@
-from madlib import greeting
+from madlib import greeting, run , word_open
 
 def test_import():
     """ First test to make sure import is valid
     """
     assert True is True
+
+
+def test_run():
+    """ tests to see if terminal call runs print
+
+    """
+    assert  "Hello welcome to our MadLib game."
+
+    assert contents = type(text)
 
 
 #  need to test input from user
