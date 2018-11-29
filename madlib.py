4597525f0f917eb45469a0417bd1d4fea147e0fb
mthwbrwn <mthwbrwn@gmail.com>
2018-11-28 19:09:14 -0800

    outline of work fleshed out

---
+++ madlib.py
@@ -0,0 +1,23 @@
+import json
+
+# need to parse questions and serialize the users questions into a answerlist
+def word_generator():
+    """ word generator
+    """
+def greeting():
+    pass
+
+
+# need an out going message stating game and asking questions
+
+print('Hello welcome to our MadLib game.')
+print('You will be asked a series of questions.')
+print('Please reply to each question once prompted.')
+print('Once you are done a MadLib page will be returned to you')
+print ('Lets get started')
+
+word_generator():
+
+
+
+# need to read text back to user with user inputs in place of {questions}
---
+++ test_madlib.py
@@ -0,0 +1,15 @@
+from madlib import greeting
+
+def test_import():
+    """ First test to make sure import is valid
+    """
+    assert True is True
+
+
+#  need to test input from user
+
+#  need to test list of user answers
+
+#  need to test output of answers and text
+#
+#
